0fbd5f8c23cb79f128fd8fc1ae228d064f76c1b1
ronaldofernandus <ronaldofernandus10@gmail.com>
2022-05-19 11:01:04 +0700

    frontend

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,73 +1,73 @@
 import React from "react";
 import { bg } from "../../assets";
 import "./style.css";
-
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-6 text-center mb-5">
-          <h2 class="heading-section">Login #09</h2>
-        </div>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6 text-center mb-5"></div>
       </div>
-      <div class="row justify-content-center">
-        <div class="col-md-6 col-lg-4">
-          <div class="login-wrap py-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6 col-lg-4">
+          <div className="login-wrap py-5">
             <div
-              class="img d-flex align-items-center justify-content-center"
+              className="img d-flex align-items-center justify-content-center"
               style={{ backgroundImage: `url(${bg})` }}
             ></div>
-            <h3 class="text-center mb-0">Welcome</h3>
-            <p class="text-center">Sign in by entering the information below</p>
-            <form action="#" class="login-form">
-              <div class="form-group">
-                <div class="icon d-flex align-items-center justify-content-center">
-                  <span class="fa fa-user"></span>
+            <h3 className="text-center mb-0">Welcome</h3>
+            <p className="text-center">
+              Sign in by entering the information below
+            </p>
+            <form action="#" className="login-form">
+              <div className="form-group">
+                <div className="icon d-flex align-items-center justify-content-center">
+                  <span className="fa fa-user"></span>
                 </div>
                 <input
                   style={{
                     marginLeft: "35px",
                   }}
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   placeholder="Username"
                   required
                 />
               </div>
-              <div class="form-group">
-                <div class="icon d-flex align-items-center justify-content-center">
-                  <span class="fa fa-lock"></span>
+              <div className="form-group">
+                <div className="icon d-flex align-items-center justify-content-center">
+                  <span className="fa fa-lock"></span>
                 </div>
                 <input
-                style={{
-                  marginLeft: "35px",
-                }}
+                  style={{
+                    marginLeft: "35px",
+                  }}
                   type="password"
-                  class="form-control"
+                  className="form-control"
                   placeholder="Password"
                   required
                 />
               </div>
-              <div class="form-group d-md-flex" >
-                <div class="w-100 text-md-right">
-                  <a href="#">Forgot Password</a>
+              <div className="form-group d-md-flex">
+                <div className="w-100 text-md-right">
+                  <Link to="#">Forgot Password</Link>
                 </div>
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <button
-                style={{
-                  marginLeft: "35px",
-                }}
+                  style={{
+                    marginLeft: "35px",
+                  }}
                   type="submit"
-                  class="btn form-control btn-primary rounded submit px-3"
+                  className="btn form-control btn-primary rounded submit px-3"
                 >
-                Login
+                  Login
                 </button>
               </div>
             </form>
-            <div class="w-100 text-center mt-4 text">
-              <p class="mb-0">Don't have an account?</p>
-              <a href="#">Sign Up</a>
+            <div className="w-100 text-center mt-4 text">
+              <p className="mb-0">Don't have an account?</p>
+              <Link to="/register">Sign Up</Link>
             </div>
           </div>
         </div>
