d27053f0949400f95af6fbd7aa1d14b80b7e39a4
ronaldofernandus <ronaldofernandus10@gmail.com>
2022-05-20 13:55:25 +0700

    frontend

--- frontend/src/config/Routerr/index.js
+++ frontend/src/config/Routerr/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Login, Register, MainApp } from "../../pages";
 
 const Routerr = () => {
   return (
-    <Router>
+    
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<MainApp />} />
       </Routes>
-    </Router>
+    
   );
 };
 
