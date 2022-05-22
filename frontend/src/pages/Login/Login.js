3a07d2b43847b4a5abdae1b9f6b81b41f9111dee
ronaldofernandus <ronaldofernandus10@gmail.com>
2022-05-23 00:06:38 +0700

    full

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -35,60 +35,60 @@ const Login = () => {
   };
 
   return (
-    <div class="limiter">
+    <div className="limiter">
       <div
-        class="container-login100"
+        className="container-login100"
         src={bg}
         // style="background-image: url('images/bg-01.jpg');"
       >
-        <div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
-          <form class="login100-form validate-form flex-sb flex-w">
-            <span class="login100-form-title p-b-53">Sign In With</span>
+        <div className="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
+          <form className="login100-form validate-form flex-sb flex-w">
+            <span className="login100-form-title p-b-53">Sign In With</span>
 
-            <a href="#" class="btn-face m-b-20">
-              <i class="fa fa-facebook-official"></i>
+            <a href="#" className="btn-face m-b-20">
+              <i className="fa fa-facebook-official"></i>
               Facebook
             </a>
 
-            <a href="#" class="btn-google m-b-20">
+            <a href="#" className="btn-google m-b-20">
               <img src={google} alt="GOOGLE" />
               Google
             </a>
 
-            <div class="p-t-31 p-b-9">
-              <span class="txt1">Username</span>
+            <div className="p-t-31 p-b-9">
+              <span className="txt1">Username</span>
             </div>
             <div
-              class="wrap-input100 validate-input"
+              className="wrap-input100 validate-input"
               data-validate="Username is required"
             >
-              <input class="input100" type="text" name="username" />
-              <span class="focus-input100"></span>
+              <input className="input100" type="text" name="username" />
+              <span className="focus-input100"></span>
             </div>
 
-            <div class="p-t-13 p-b-9">
-              <span class="txt1">Password</span>
+            <div className="p-t-13 p-b-9">
+              <span className="txt1">Password</span>
 
-              <a href="#" class="txt2 bo1 m-l-5">
+              <a href="#" className="txt2 bo1 m-l-5">
                 Forgot?
               </a>
             </div>
             <div
-              class="wrap-input100 validate-input"
+              className="wrap-input100 validate-input"
               data-validate="Password is required"
             >
-              <input class="input100" type="password" name="pass" />
-              <span class="focus-input100"></span>
+              <input className="input100" type="password" name="pass" />
+              <span className="focus-input100"></span>
             </div>
 
-            <div class="container-login100-form-btn m-t-17">
-              <button class="login100-form-btn">Sign In</button>
+            <div className="container-login100-form-btn m-t-17">
+              <button className="login100-form-btn">Sign In</button>
             </div>
 
-            <div class="w-full text-center p-t-55">
-              <span class="txt2">Not a member?</span>
+            <div className="w-full text-center p-t-55">
+              <span className="txt2">Not a member?</span>
 
-              <a href="#" class="txt2 bo1">
+              <a href="#" className="txt2 bo1">
                 Sign up now
               </a>
             </div>
